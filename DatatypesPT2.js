f0a5e8a99dba4a85749ce1b245ebdd6f436e5664
McNoHam14 <McNoHam14@gmail.com>
2022-11-29 15:59:46 +0000

    2nd save

--- DatatypesPT2.js
+++ DatatypesPT2.js
@@ -7,32 +7,51 @@
 
 /* WRITE YOUR ANSWER HERE */
 
-let gender = "Female" || "Male"
-console.log(gender)
+// if statement
 
-let ifMale = "Female"
 
-if (ifMale === "Male") {
-console.log("Male")
+
+
+
+/*
+let gender = "Female" || "Male";
+console.log(gender);
+
+ifMale = "Male";
+console.log(ifMale);
+
+if (ifMale = "female") {
+console.log("Male");
 }
 else {
-console.log("Female")
+console.log("Female");
 }
 
-/* const isMale = "female"
+// ternary operator
+let isMale1 = "Female";
+(isMale1 = "Male") ? console.log("Male") : console.log("Female");
+*/
+
+/*
+const shoeSize = 4;
+
+(shoeSize >= 6) ? console.log("Adult Shoes") : console.log("Kids Shoes");
+
+ const isMale = "female"
 console.log(isMale)
 let gender = "Male" || "Female"
 
-if (isMale === "Male") {
-    console.log("Male")
-}
-else {
-    console.log("Female")
-}
+// (isMale = gender) ? console.log("Male") : console.log("female");
 */
 
 
-// (isMale = gender) ? console.log("Male") : console.log("female");
+
+
+
+
+
+
+
 
 /*EX 2
  Write a piece of code for checking if, given two integers, the value of one of them is 8 or if their addition or subtraction is equal to 8.
@@ -40,11 +59,32 @@ else {
 
 /* WRITE YOUR ANSWER HERE */
 
+let int1 = 4;
+let int2 = 4;
+
+if 
+(int1 === 8){
+    console.log("True");
+} else if (int2 === 8) {
+    console.log("True");
+} else if (int1 + int2 === 8) {
+    console.log("True");
+} else {
+console.log("False");
+}
+
+
 /*EX 3
  Create a variable and assign to it the concatenation of two strings.
 */
 
 /* WRITE YOUR ANSWER HERE */
+
+let string1 = "Apple";
+let string2 = "Orange";
+
+let conCat = string1 + string2;
+console.log(conCat);
 
 /*EX 4
  Create three variables and assign a numerical value to each one of them. 
@@ -54,11 +94,56 @@ else {
 
 /* WRITE YOUR ANSWER HERE */
 
+let firstNum = 1;
+let secondNum = 2;
+let thirdNum = 3;
+
+if (firstNum > secondNum && thirdNum)
+    {
+    console.log("1st Number:", firstNum)
+} else if (secondNum > firstNum && thirdNum) {
+    console.log("1st Number:", secondNum)
+} else if (thirdNum > firstNum && secondNum){
+    console.log("1st Number:", thirdNum)
+}
+
+/*
+if (firstNum > secondNum && firstNum < thirdNum)
+    {
+    console.log("2nd Number:", firstNum)
+    } else if ((SecondNum > firstNum && thirdNum < thirdNum)) {
+    console.log("1st Number:", secondNum)
+} else if (thirdNum > firstNum && secondNum){
+    console.log("1st Number:", thirdNum)
+}
+*/
+
+/*
+} else if (secondNum > firstNum && thirdNum) {
+    console.log("1st Number:", secondNum)
+} else if (thirdNum > firstNum && secondNum){
+    console.log("1st Number:", thirdNum)
+}
+
+*/
+
+
+
+
+
+
+
 /*EX 5
  Write a piece of code for finding the average of two given integers.
 */
 
 /* WRITE YOUR ANSWER HERE */
+
+let egAverage1 = 4;
+let egAverage2 = 8;
+
+egAverage3 = ((egAverage1 * egAverage2) / 2)
+console.log(egAverage3);
 
 /*EX 6
  Write a piece of code for finding the longest of two given strings.
